e622adc325e13b1906a8bbced6cf7aa775858837
snow <4344235+emlog@users.noreply.github.com>
2026-04-17 11:02:37 +0800

    update skill-lake to 1.1.3

--- Casks/skill-lake.rb
+++ Casks/skill-lake.rb
@@ -1,6 +1,6 @@
 cask "skill-lake" do
   version "1.1.3"
-  sha256 "c53947c80ed48191cff0417cfe5b7612f1b5f21686021530977ed35366399b26"
+  sha256 "cbf90a50bcbaf35e702cf8f9f29d385a60ee96edb57dab514388f828222fa260"
 
   url "https://github.com/emlog/skill-lake/releases/download/1.1.3/SkillLake-1.1.3.dmg"
   name "Skill Lake"
@@ -8,4 +8,10 @@ cask "skill-lake" do
   homepage "https://github.com/emlog/skill-lake"
 
   app "Skill Lake.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.emlog.skillLake",
+    "~/Library/Preferences/com.emlog.skillLake.plist",
+    "~/Library/Saved Application State/com.emlog.skillLake.savedState",
+  ]
 end
