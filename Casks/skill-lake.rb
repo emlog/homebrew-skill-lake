fdedd2782503e642b1dd176954e705cdf6028a45
snow <4344235+emlog@users.noreply.github.com>
2026-04-17 10:54:36 +0800

    update skill-lake to 1.1.3

---
+++ Casks/skill-lake.rb
@@ -0,0 +1,11 @@
+cask "skill-lake" do
+  version "1.1.3"
+  sha256 "c53947c80ed48191cff0417cfe5b7612f1b5f21686021530977ed35366399b26"
+
+  url "https://github.com/emlog/skill-lake/releases/download/1.1.3/SkillLake-1.1.3.dmg"
+  name "Skill Lake"
+  desc "A local AI agent skill manager app for macOS"
+  homepage "https://github.com/emlog/skill-lake"
+
+  app "Skill Lake.app"
+end
